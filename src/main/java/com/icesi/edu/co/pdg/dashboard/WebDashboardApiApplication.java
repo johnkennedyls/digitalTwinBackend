ef31d56317fd0eafb8a4322608adf4f098786842
YoNoSoySantiago-SB <santiago.hurtado@segurosbolivar.com>
2023-04-19 00:02:07 -0500

    Initial Commit

---
+++ src/main/java/com/icesi/edu/co/pdg/dashboard/WebDashboardApiApplication.java
@@ -0,0 +1,13 @@
+package com.icesi.edu.co.pdg.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDashboardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDashboardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icesi/edu/co/pdg/dashboard/WebDashboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icesi.edu.co.pdg.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDashboardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

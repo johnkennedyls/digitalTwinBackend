f8759ee5c9c7e9345014bff0782ac4f19e17c9d9
Cpasuy <carolinapasuy@hotmail.com>
2023-05-06 11:44:39 -0500

    New Project

---
+++ src/main/java/com/icesi/edu/co/pdg/dashboard/I40webBackApplication.java
@@ -0,0 +1,13 @@
+package com.icesi.edu.co.pdg.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I40webBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(I40webBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icesi/edu/co/pdg/dashboard/I40webBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icesi.edu.co.pdg.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I40webBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
